db1edf66e6f5a0dbacc2ebc26e653979380527dc
dinildamsith <dinildamsith52@gmail.com>
2024-06-25 23:38:47 +0530

    user dto and entity added

---
+++ user-service/src/main/java/lk/ijse/user_service/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package lk.ijse.user_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity implements SuperEntity{
+    @Id
+    private String userId;
+    private String userName;
+    private String password;
+
+
+}
