df7d8406e22ba747c87c448bc63e35c5c5e66ed5
dinildamsith <dinildamsith52@gmail.com>
2024-06-27 03:22:36 +0530

    vehicle dto and entity added

---
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/entity/VehicleEntity.java
@@ -0,0 +1,21 @@
+package lk.ijse.vehicle_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "vehicle")
+public class VehicleEntity implements SuperEntity{
+    @Id
+    private String vehicleId;
+    private String vehicleNumber;
+    private String vehicleType;
+
+}
