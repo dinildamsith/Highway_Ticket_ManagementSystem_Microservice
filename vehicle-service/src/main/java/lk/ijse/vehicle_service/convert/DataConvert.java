8f99bc2a3a664bb8a7e210478c4fc16b4301b8e2
dinildamsith <dinildamsith52@gmail.com>
2024-06-27 03:25:09 +0530

    data convert added model mapper

---
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/convert/DataConvert.java
@@ -0,0 +1,24 @@
+package lk.ijse.vehicle_service.convert;
+
+
+import lk.ijse.vehicle_service.dto.VehicleDTO;
+import lk.ijse.vehicle_service.entity.VehicleEntity;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataConvert {
+
+    private final ModelMapper modelMapper;
+
+    public VehicleDTO vehicleEntityConvertVehicleDTO(VehicleEntity vehicleEntity){
+        return modelMapper.map(vehicleEntity, VehicleDTO.class);
+    }
+
+    public VehicleEntity vehicleDTOConvertVehicleEntity(VehicleDTO vehicleDTO){
+        return modelMapper.map(vehicleDTO, VehicleEntity.class);
+    }
+
+}
