90edb5d658834bd9bce7e12dae598f4b3ac1e4a9
dinildamsith <dinildamsith52@gmail.com>
2024-06-26 23:40:28 +0530

    data convert class added

--- user-service/src/main/java/lk/ijse/user_service/convert/DataConvert.java
+++ user-service/src/main/java/lk/ijse/user_service/convert/DataConvert.java
@@ -1,4 +1,22 @@
 package lk.ijse.user_service.convert;
 
+import lk.ijse.user_service.dto.UserDTO;
+import lk.ijse.user_service.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DataConvert {
+
+    private final ModelMapper modelMapper;
+
+    public UserDTO userEntityConvertUserDTO(UserEntity userEntity){
+        return modelMapper.map(userEntity, UserDTO.class);
+    }
+
+    public UserEntity userDTOConvertUserEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
 }
