0f34034dcea6ac7d83a37c3b66daa5007097136b
dinildamsith <dinildamsith52@gmail.com>
2024-06-27 18:58:03 +0530

    ticket creation done

--- ticket-service/src/main/java/lk/ijse/ticket_service/controller/TicketController.java
+++ ticket-service/src/main/java/lk/ijse/ticket_service/controller/TicketController.java
@@ -1,6 +1,10 @@
 package lk.ijse.ticket_service.controller;
 
+import lk.ijse.ticket_service.dto.TicketDTO;
+import lk.ijse.ticket_service.service.TicketServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/ticketService")
 public class TicketController {
 
+    @Autowired
+    TicketServices ticketServices;
+
     @PostMapping
     public void checkHealth(){
         System.out.println("Ticket Worked...");
+    }
+
+    @PostMapping
+    @RequestMapping("/createTicket")
+    public String createTicket(@RequestBody TicketDTO ticketDTO){
+        String ticket = ticketServices.ticketCreate(ticketDTO);
+        return ticket;
     }
 }
--- ticket-service/src/main/java/lk/ijse/ticket_service/service/impl/TicketServiceIMPL.java
+++ ticket-service/src/main/java/lk/ijse/ticket_service/service/impl/TicketServiceIMPL.java
@@ -1,11 +1,47 @@
 package lk.ijse.ticket_service.service.impl;
 
+import jakarta.transaction.Transactional;
+import lk.ijse.ticket_service.convert.DataConvert;
+import lk.ijse.ticket_service.dao.TicketRepo;
 import lk.ijse.ticket_service.dto.TicketDTO;
+import lk.ijse.ticket_service.entity.TicketEntity;
 import lk.ijse.ticket_service.service.TicketServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class TicketServiceIMPL implements TicketServices {
+
+    @Autowired
+    TicketRepo ticketRepo;
+
+    @Autowired
+    DataConvert dataConvert;
+
+    Logger logger = LoggerFactory.getLogger(TicketServiceIMPL.class);
+
+
     @Override
     public String ticketCreate(TicketDTO ticketDTO) {
+
+        if (ticketRepo.existsById(ticketDTO.getTicketId())){
+            logger.info("This Id Have already Ticket");
+        }else {
+            TicketEntity ticket = dataConvert.ticketDTOConvertTicketEntity(ticketDTO);
+            ticket.setPrice(ticket.getOneKmPrice() * ticket.getTotalKm());
+            ticketRepo.save(ticket);
+            return  ticket.getTicketId() + "\n" +
+                    ticket.getEntranceLocation() + "\n" +
+                    ticket.getExitLocation() + "\n" +
+                    ticket.getStatus() + "\n" +
+                    ticket.getTicketIssueTime() + "\n" +
+                    ticket.getOneKmPrice() + "\n" +
+                    ticket.getTotalKm() + "\n" +
+                    ticket.getPrice();
+        }
         return null;
     }
 
