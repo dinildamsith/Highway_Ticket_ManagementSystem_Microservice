d0e02b38fa649f666cc440e67a9c888a55c9493c
dinildamsith <dinildamsith52@gmail.com>
2024-07-16 18:33:08 +0530

    vehicle register before user exits check

--- vehicle-service/src/main/java/lk/ijse/vehicle_service/config/config.java
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/config/config.java
@@ -3,11 +3,17 @@ package lk.ijse.vehicle_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class config {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
     }
 }
--- vehicle-service/src/main/java/lk/ijse/vehicle_service/service/impl/VehicleServiceIMPL.java
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/service/impl/VehicleServiceIMPL.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -21,41 +22,57 @@ public class VehicleServiceIMPL implements VehicleServices {
 
     @Autowired
     VehicleRepo vehicleRepo;
-
     @Autowired
     DataConvert dataConvert;
+    @Autowired
+    RestTemplate restTemplate;
 
     Logger logger = LoggerFactory.getLogger(VehicleServiceIMPL.class);
 
     @Override
     public String registerVehicle(@RequestBody  VehicleDTO vehicleDTO) {
 
+        boolean userExists = restTemplate.getForObject("http://localhost:8082/api/v1/userService/userExists/"+vehicleDTO.getUsers().getUserId(), Boolean.class);
 
-        if (vehicleRepo.existsById(vehicleDTO.getVehicleId())){
-            logger.info("This Id Have Already register Vehicle");
-            return "This Id Have Already register Vehicle";
-        }else{
-            vehicleRepo.save(dataConvert.vehicleDTOConvertVehicleEntity(vehicleDTO));
-            logger.info("Vehicle Register Done");
-            return "Vehicle Register Done";
+        if (userExists){
+            if (vehicleRepo.existsById(vehicleDTO.getVehicleId())){
+                logger.info("This Id Have Already register Vehicle");
+                return "This Id Have Already register Vehicle";
+            }else{
+                vehicleRepo.save(dataConvert.vehicleDTOConvertVehicleEntity(vehicleDTO));
+                logger.info("Vehicle Register Done");
+                return "Vehicle Register Done";
+            }
+        }else {
+            logger.info("There is no user for this id");
+            return "There is no user for this id";
         }
     }
 
     @Override
     public String updateVehicle(String updateVehicleId, VehicleDTO vehicleDTO) {
 
-        if (vehicleRepo.existsById(updateVehicleId)){
-            VehicleEntity updateVehicleEntity = vehicleRepo.findById(updateVehicleId).orElse(null);
-            updateVehicleEntity.setVehicleNumber(vehicleDTO.getVehicleNumber());
-            updateVehicleEntity.setVehicleType(vehicleDTO.getVehicleType());
-            updateVehicleEntity.setUsers(vehicleDTO.getUsers());
+        boolean userExists = restTemplate.getForObject("http://localhost:8082/api/v1/userService/userExists/"+vehicleDTO.getUsers().getUserId(), Boolean.class);
 
-            vehicleRepo.save(updateVehicleEntity);
-            return "Vehicle Update Success";
+        if (userExists){
+            if (vehicleRepo.existsById(updateVehicleId)){
+                VehicleEntity updateVehicleEntity = vehicleRepo.findById(updateVehicleId).orElse(null);
+                updateVehicleEntity.setVehicleNumber(vehicleDTO.getVehicleNumber());
+                updateVehicleEntity.setVehicleType(vehicleDTO.getVehicleType());
+                updateVehicleEntity.setUsers(vehicleDTO.getUsers());
+
+                vehicleRepo.save(updateVehicleEntity);
+                return "Vehicle Update Success";
+            }else{
+                logger.info("This Id Have No Vehicles");
+                return "This Id Have No Vehicles";
+            }
         }else{
-            logger.info("This Id Have No Vehicles");
-            return "This Id Have No Vehicles";
+            logger.info("There is no user for this id");
+            return "There is no user for this id";
         }
+
+
     }
 
     @Override
