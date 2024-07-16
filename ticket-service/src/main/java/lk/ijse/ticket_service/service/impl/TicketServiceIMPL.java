786a24e2bb309dc64409c45cd2c4dda9eea02a69
dinildamsith <dinildamsith52@gmail.com>
2024-07-16 18:50:18 +0530

    added ticket create before check vehicle id exits

--- ticket-service/src/main/java/lk/ijse/ticket_service/service/impl/TicketServiceIMPL.java
+++ ticket-service/src/main/java/lk/ijse/ticket_service/service/impl/TicketServiceIMPL.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional
@@ -17,9 +18,10 @@ public class TicketServiceIMPL implements TicketServices {
 
     @Autowired
     TicketRepo ticketRepo;
-
     @Autowired
     DataConvert dataConvert;
+    @Autowired
+    RestTemplate restTemplate;
 
     Logger logger = LoggerFactory.getLogger(TicketServiceIMPL.class);
 
@@ -27,50 +29,66 @@ public class TicketServiceIMPL implements TicketServices {
     @Override
     public String ticketCreate(TicketDTO ticketDTO) {
 
-        if (ticketRepo.existsById(ticketDTO.getTicketId())){
-            logger.info("This Id Have already Ticket");
-            return "This Id Have already Ticket";
-        }else {
-            TicketEntity ticket = dataConvert.ticketDTOConvertTicketEntity(ticketDTO);
-            ticket.setPrice(ticket.getOneKmPrice() * ticket.getTotalKm());
-            ticketRepo.save(ticket);
-            return  "Ticket ID: " + ticket.getTicketId() + "\n" +
-                    "Entrance Location: " + ticket.getEntranceLocation() + "\n" +
-                    "Exit Location: " + ticket.getExitLocation() + "\n" +
-                    "Status: " + ticket.getStatus() + "\n" +
-                    "Ticket Issue Time: " + ticket.getTicketIssueTime() + "\n" +
-                    "One Km Price: " + ticket.getOneKmPrice() + "\n" +
-                    "Total Km: " + ticket.getTotalKm() + "\n" +
-                    "Price: " + ticket.getPrice();
+        boolean vehicleExists = restTemplate.getForObject("http://localhost:8082/api/v1/vehicleService/vehicleExists/"+ticketDTO.getVehicle().getVehicleId(), Boolean.class);
+
+        if (vehicleExists){
+            if (ticketRepo.existsById(ticketDTO.getTicketId())){
+                logger.info("This Id Have already Ticket");
+                return "This Id Have already Ticket";
+            }else {
+                TicketEntity ticket = dataConvert.ticketDTOConvertTicketEntity(ticketDTO);
+                ticket.setPrice(ticket.getOneKmPrice() * ticket.getTotalKm());
+                ticketRepo.save(ticket);
+                return  "Ticket ID: " + ticket.getTicketId() + "\n" +
+                        "Entrance Location: " + ticket.getEntranceLocation() + "\n" +
+                        "Exit Location: " + ticket.getExitLocation() + "\n" +
+                        "Status: " + ticket.getStatus() + "\n" +
+                        "Ticket Issue Time: " + ticket.getTicketIssueTime() + "\n" +
+                        "One Km Price: " + ticket.getOneKmPrice() + "\n" +
+                        "Total Km: " + ticket.getTotalKm() + "\n" +
+                        "Price: " + ticket.getPrice();
+            }
+        }else{
+            return "There is no Vehicle for this id";
         }
+
+
     }
 
     @Override
     public String updateTicket(String updateTicketId , TicketDTO ticketDTO) {
 
-        if (ticketRepo.existsById(updateTicketId)){
-            TicketEntity updateTicket = ticketRepo.findById(updateTicketId).orElse(null);
-            updateTicket.setEntranceLocation(ticketDTO.getEntranceLocation());
-            updateTicket.setExitLocation(ticketDTO.getExitLocation());
-            updateTicket.setStatus(ticketDTO.getStatus());
-            updateTicket.setTicketIssueTime(ticketDTO.getTicketIssueTime());
-            updateTicket.setOneKmPrice(ticketDTO.getOneKmPrice());
-            updateTicket.setTotalKm(ticketDTO.getTotalKm());
-            updateTicket.setPrice(ticketDTO.getOneKmPrice() * ticketDTO.getTotalKm());
-            updateTicket.setVehicle(ticketDTO.getVehicle());
-            ticketRepo.save(updateTicket);
-            return  "Ticket ID: " + updateTicket.getTicketId() + "\n" +
-                    "Entrance Location: " + updateTicket.getEntranceLocation() + "\n" +
-                    "Exit Location: " + updateTicket.getExitLocation() + "\n" +
-                    "Status: " + updateTicket.getStatus() + "\n" +
-                    "Ticket Issue Time: " + updateTicket.getTicketIssueTime() + "\n" +
-                    "One Km Price: " + updateTicket.getOneKmPrice() + "\n" +
-                    "Total Km: " + updateTicket.getTotalKm() + "\n" +
-                    "Price: " + updateTicket.getPrice();
+        boolean vehicleExists = restTemplate.getForObject("http://localhost:8082/api/v1/vehicleService/vehicleExists/"+ticketDTO.getVehicle().getVehicleId(), Boolean.class);
+
+        if (vehicleExists){
+            if (ticketRepo.existsById(updateTicketId)){
+                TicketEntity updateTicket = ticketRepo.findById(updateTicketId).orElse(null);
+                updateTicket.setEntranceLocation(ticketDTO.getEntranceLocation());
+                updateTicket.setExitLocation(ticketDTO.getExitLocation());
+                updateTicket.setStatus(ticketDTO.getStatus());
+                updateTicket.setTicketIssueTime(ticketDTO.getTicketIssueTime());
+                updateTicket.setOneKmPrice(ticketDTO.getOneKmPrice());
+                updateTicket.setTotalKm(ticketDTO.getTotalKm());
+                updateTicket.setPrice(ticketDTO.getOneKmPrice() * ticketDTO.getTotalKm());
+                updateTicket.setVehicle(ticketDTO.getVehicle());
+                ticketRepo.save(updateTicket);
+                return  "Ticket ID: " + updateTicket.getTicketId() + "\n" +
+                        "Entrance Location: " + updateTicket.getEntranceLocation() + "\n" +
+                        "Exit Location: " + updateTicket.getExitLocation() + "\n" +
+                        "Status: " + updateTicket.getStatus() + "\n" +
+                        "Ticket Issue Time: " + updateTicket.getTicketIssueTime() + "\n" +
+                        "One Km Price: " + updateTicket.getOneKmPrice() + "\n" +
+                        "Total Km: " + updateTicket.getTotalKm() + "\n" +
+                        "Price: " + updateTicket.getPrice();
+            }else{
+                logger.info("This Id Have No Ticket");
+                return "This Id Have No Ticket";
+            }
         }else{
-            logger.info("This Id Have No Ticket");
-            return "This Id Have No Ticket";
+            return "There is no Vehicle for this id";
         }
+
+
     }
 
     @Override
